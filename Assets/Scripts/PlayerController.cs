ff23a1ba1da499515a8c8d4aa9658cd43fe8e395
dsoto015 <dsoto015@yahoo.com>
2025-12-28 11:48:39 -0800

    added score and text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+    public GameObject winTextObject;
+
     private Rigidbody _rigidBody; 
     private float _movementX;
     private float _movementY;
-    public float speed = 0;
+    private int _count = 0;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigidBody = GetComponent <Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void FixedUpdate() 
@@ -28,5 +38,33 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>(); 
         _movementX = movementVector.x; 
         _movementY = movementVector.y; 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);    
+            _count++;
+            SetCountText();
+            if(CheckWinCondition(5))
+            {
+                winTextObject.SetActive(true);
+            }
+        }
+    }
+
+    bool CheckWinCondition(int pickUpCount)
+    {
+        if(_count < pickUpCount)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void SetCountText()
+    {
+        countText.text = $"Count {_count}";
     }
 }
